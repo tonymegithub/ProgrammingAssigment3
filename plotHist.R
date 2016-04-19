c9c88a3b7821963af4ee52827ff1fb49826d407d
tonymegithub <junmingmei@gmail.com>
2016-04-18 23:14:35 -0400

    first commit

---
+++ plotHist.R
@@ -0,0 +1,6 @@
+plotMortarityRate <- function()
+{
+  outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  outcome[, 11] <- as.numeric(outcome[, 11])
+  hist(outcome[, 11])
+}
